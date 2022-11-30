d95c273775792ebfd9a7f06b138a205ba0c5afc6
Simone Acuti <acutisimone@gmail.com>
2022-11-30 15:02:38 +0100

    aggiunta tutorato08

--- tutorato/tutorato08.c
+++ tutorato/tutorato08.c
@@ -2,9 +2,72 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
+int ricercaStr(char *s1, char *s2, char *occorrenze){
+    int i, j, k = 0;
+    int elementi = 0;
+    int trovato = 0, inserito = 0;
+    char tmp[100];
+
+
+    for(i = 0; s1[i] != '\0'; i++){
+        tmp[i] = s1[i];
+    }
+
+    for(i = 0; s1[i] != '\0'; i++){
+        trovato = 0;
+        inserito = 0;
+        for(j = i + 1; s1[j] != '\0'; j++){
+            if(s1[i] == s1[j] && s1[i] != '0'){
+                s1[j] = '0';
+                trovato = 1;
+            }
+            if(trovato == 1 && inserito == 0){
+                occorrenze[k] = s1[i];
+                k++;
+                inserito = 1;
+            }
+        }
+    }
+
+    for(i = 0; tmp[i] != '\0'; i++){
+        s1[i] = tmp[i];
+    }
+
+    for(i = 0; s1[i] != '\0'; i++){
+        for(k = 0; occorrenze[k] != '\0'; k++){
+            if(s1[i] == occorrenze[k]){
+                for(j = 0; s2[j] != '\0'; j++){
+                    if(s2[j] == s1[i]){
+                        s1[i] = '0';
+                        elementi++;
+                    }
+                }
+            }
+        }
+    }
+
+    return elementi;
+}
+
+
 void duplicati(){
     char s1[100];
     char s2[100];
+    int elementi;
+    char occorrenze[100];
+
+    printf("\nInserisci le due stringhe: ");
+    scanf("%s",s1);
+    scanf("%s",s2);
+
+    elementi = ricercaStr(s1, s2, occorrenze);
+
+    printf("le occorrenze: %s",occorrenze);
+    printf("\nElementi rimossi: %d",elementi);
+    printf("\nLa tua stringa: %s", s1);
+
 }
 
 
@@ -62,7 +125,7 @@ int main(){
     int scelta;
 
     printf("\n1. occorreze\n");
-    printf("\n2. rimuovi duplicati\n");
+    printf("2. rimuovi duplicati\n");
     printf(":");
     scanf("%d",&scelta);
 
@@ -73,6 +136,9 @@ int main(){
         break;
     
     case 2:
+        system("clear");
+        duplicati();
+        break;
 
     default:
         break;
