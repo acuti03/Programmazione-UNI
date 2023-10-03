07d14237e0ce4e2bd4bc25f433f60613c9c9304e
Simone Acuti <acutisimone@gmail.com>
2023-10-03 20:42:25 +0200

    Update counter

--- java/lezioni/01_Basi_OOP/Counter.java
+++ java/lezioni/01_Basi_OOP/Counter.java
@@ -1,23 +1,32 @@
 class Counter{
-    int i;
+	double i;
 
-    Counter(int i){
-       this.i = i; 
-    }
+	Counter(int i){
+		this.i = i; 
+	}
+	Counter(double i){
+		this.i = i;
+	}
 
-    void reset(){
-        i = 0;
-    }
-    void incrementa(){
-        i++;
-    }
-    void decrementa(){
-        i--;
-    }
-    void stampa(){
-        System.out.println(i);
-    }
-    void copy(Counter c){
-        i = c.i;
-    }
+	void reset(){
+		i = 0;
+	}
+	void inc(){
+		i++;
+	}
+	void inc(int n){
+		i += n;
+	}
+	void dec(){
+		i--;
+	}
+	void print(){
+		System.out.println(i);
+	}
+	void copy(Counter c){
+		i = c.i;
+	}
+	boolean equals(Counter c){
+		return i == c.i;
+	}
 }
--- java/lezioni/01_Basi_OOP/Main.java
+++ java/lezioni/01_Basi_OOP/Main.java
@@ -1,8 +1,17 @@
 class Main{
-    public static void main(String[] args){
-        Counter cont1 = new Counter(1);
+	public static void main(String[] args){
+		Counter cont1 = new Counter(1);
+		Counter cont2 = new Counter(0);
+		Counter cont3 = new Counter(0.5F);
+		boolean b1;
 
-        cont1.incrementa();
-        cont1.stampa();
-    }
+		cont1.inc();
+		cont2.inc();
+		//cont2.copy(cont1);
+		b1 = cont1.equals(cont2);
+		System.out.println(b1);
+		cont3.print();
+
+		cont2.print();
+	}
 }
